f574b41fa0af1aaddeda8a29862d4c235dd89f1d
Elena Zhukova <ellena.zhukof@gmail.com>
2026-07-11 09:09:49 +0300

    back end completed

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+  def new
+    @movies = Movie.all
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path([@list, @bookmark])
+    else
+      @movies = Movie.all
+      render :new, status: 422
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path([@list, @bookmark]), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
