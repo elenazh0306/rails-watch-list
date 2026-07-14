d3711e102872d28b91e726ac8a0f063a2a829ccf
Elena Zhukova <ellena.zhukof@gmail.com>
2026-07-14 15:53:24 +0300

    list model corrected

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  has_one_attached :photo
 
   validates :title, uniqueness: true, presence: true
   validates :overview, uniqueness: true, presence: true
